ddccf14eb4bf5c4886a45ed7d4085cb15ab62a1e
Dllite <dilanzambou2@gmail.com>
2023-01-20 16:58:56 +0100

    Debut du projet

---
+++ boutique.php
@@ -0,0 +1,8 @@
+<?php 
+
+    require_once 'view/header_view.php';
+
+    require_once 'view/view_shop.php';
+
+    require_once 'view/footer_view.php';
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+    require_once 'view/header_view.php';
+
+    require_once 'view/body_view.php';
+
+    require_once 'view/footer_view.php';
