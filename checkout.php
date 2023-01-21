e2b98dd56383cc748cb7142c57a896f8c0a43b4e
Dllite <dilanzambou2@gmail.com>
2023-01-21 13:56:27 +0100

    Fin du decoupage mvc

--- checkout.php
+++ checkout.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
     require_once "view/header_view.php";
 
-    require_once "view/detail_view.php";
+    require_once "view/checkout_view.php";
 
     require_once "view/footer_view.php";
 ?>
--- contact.php
+++ contact.php
@@ -1,7 +1,8 @@
 <?php 
+
     require_once "view/header_view.php";
 
-    require_once "view/detail_view.php";
+    require_once "view/contact_view.php";
 
     require_once "view/footer_view.php";
 ?>
