05ec9a7ec25c558887e9691963e7674ecb229fff
ulrich <ulrichfedjo@gmail.com>
2023-01-23 02:50:53 -0800

    boutique

--- view/view_shop.php
+++ view/view_shop.php
@@ -3,7 +3,7 @@
     <div class="row px-xl-5">
         <div class="col-12">
             <nav class="breadcrumb bg-light mb-30">
-                <a class="breadcrumb-item text-dark" href="#">Home</a>
+                <a class="breadcrumb-item text-dark" href="#">Accueil</a>
                 <a class="breadcrumb-item text-dark" href="#">Boutique</a>
                 <span class="breadcrumb-item active">liste</span>
             </nav>
